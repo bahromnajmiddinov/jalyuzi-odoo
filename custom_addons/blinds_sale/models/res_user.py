2b0afea5ad2a493d7b4ebca020fc679d7a9e8ce8
Bahrom <najmiddinovbahrom402@gmail.com>
2026-01-04 12:06:28 +0500

    ref: stats calculation fix

--- custom_addons/blinds_sale/models/res_user.py
+++ custom_addons/blinds_sale/models/res_user.py
@@ -23,36 +23,36 @@ class ResUser(models.Model):
     
     @api.depends('delivered_orders', 'total_orders')
     def _compute_delivery_success_rate(self):
-        for employee in self:
-            if employee.total_orders > 0:
-                employee.delivery_success_rate = (employee.delivered_orders / employee.total_orders) * 100
+        for user in self:
+            if user.total_orders > 0:
+                user.delivery_success_rate = (user.delivered_orders / user.total_orders) * 100
             else:
-                employee.delivery_success_rate = 0
-    
+                user.delivery_success_rate = 0
+
     @api.depends('cancelled_orders', 'total_orders')
     def _compute_cancelation_rate(self):
-        for employee in self:
-            if employee.total_orders > 0:
-                employee.cancellation_rate = (employee.cancelled_orders / employee.total_orders) * 100
+        for user in self:
+            if user.total_orders > 0:
+                user.cancellation_rate = (user.cancelled_orders / user.total_orders) * 100
             else:
-                employee.cancellation_rate = 0
-    
+                user.cancellation_rate = 0
+
     @api.depends('sale_order_ids')
     def _compute_total_orders(self):
-        for employee in self:
-            if employee.user_id and employee.user_id.sale_order_ids:
-                total_orders = len(employee.user_id.sale_order_ids)
-                pending_orders = sum(1 for order in employee.user_id.sale_order_ids if order.state == 'draft')
-                delivered_orders = sum(1 for order in employee.user_id.sale_order_ids if order.state == 'done')
-                cancelled_orders = sum(1 for order in employee.user_id.sale_order_ids if order.state == 'cancel')
+        for user in self:
+            if user.sale_order_ids:
+                total_orders = len(user.sale_order_ids)
+                pending_orders = sum(1 for order in user.sale_order_ids if order.state == 'draft')
+                delivered_orders = sum(1 for order in user.sale_order_ids if order.state == 'done')
+                cancelled_orders = sum(1 for order in user.sale_order_ids if order.state == 'cancel')
             else:
                 total_orders = pending_orders = delivered_orders = cancelled_orders = 0
                 
-            employee.total_orders = total_orders
-            employee.pending_orders = pending_orders
-            employee.delivered_orders = delivered_orders
-            employee.cancelled_orders = cancelled_orders
-    
+            user.total_orders = total_orders
+            user.pending_orders = pending_orders
+            user.delivered_orders = delivered_orders
+            user.cancelled_orders = cancelled_orders
+
     @api.depends('image_1920')
     def _compute_image_url(self):
         for user in self:
@@ -67,26 +67,25 @@ class ResUser(models.Model):
     
     @api.depends('sale_order_ids.amount_total')
     def _compute_total_sales(self):
-        for employee in self:
-            if employee.user_id and employee.user_id.sale_order_ids:
-                total_sales = sum(employee.user_id.sale_order_ids.mapped('amount_total'))
+        for user in self:
+            if user.sale_order_ids:
+                total_sales = sum(user.sale_order_ids.mapped('amount_total'))
             else:
                 total_sales = 0.0
-            employee.total_sales = total_sales
-
-
+            user.total_sales = total_sales
+            
     @api.depends('sale_order_ids.paid_amount')
     def _compute_total_payments(self):
-        for employee in self:
-            if employee.user_id and employee.user_id.sale_order_ids:
-                total_payments = sum(employee.user_id.sale_order_ids.mapped('paid_amount'))
+        for user in self:
+            if user.sale_order_ids:
+                total_payments = sum(user.sale_order_ids.mapped('paid_amount'))
             else:
                 total_payments = 0.0
-            employee.total_payments = total_payments
-
-
+            user.total_payments = total_payments
+            
     @api.depends('total_sales', 'total_payments')
     def _compute_debt_amount(self):
-        for employee in self:
-            debt = (employee.total_sales or 0.0) - (employee.total_payments or 0.0)
-            employee.sale_debt = debt
+        for user in self:
+            debt = (user.total_sales or 0.0) - (user.total_payments or 0.0)
+            user.sale_debt = debt
+            
